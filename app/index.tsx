54ddf6c5d29f8848a8d66de7f7ba7895670fc3e7
Brice NOUMI <bricenikenoumimipo@gmail.com>
2025-11-17 23:34:48 +0100

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import {ScrollView, Text, View, Image, TextInput } from "react-native";
+import React from "react";
+
+
+export default function Index() {
+  return (
+    <ScrollView>
+        <Text>Some text</Text>
+        <View>
+            <Text>Some more text</Text>
+            <Image
+                source = {{uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}}
+                style = {{width : 200, height : 200}}/>
+        </View>
+        <TextInput style={{height : 40, borderColor : 'grey', borderWidth:1 }} defaultValue={'You can type here'}/>
+    </ScrollView>
+  );
+}
